fe7ebfc9473895192407a06ff964bfbf9310670e
daniloramin <daniloramin00@hotmail.com>
2022-11-27 19:24:38 -0300

    finish code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/:value", (req, res) => {
-  return res.send({ resultado: Number(req.params.value) * 2 });
+app.get("/", (req, res) => {
+  return res.send({ resultado: Number(req.query.value) * 2 });
 });
 
 app.listen(3000, () => {
